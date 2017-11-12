cec48a90013f589e80c9ea71a4a04b819eacb2f4
ardhiesta <ardhi.mail@gmail.com>
2017-11-12 16:22:20 +0700

    first commit

---
+++ app/src/main/java/io/github/ardhiesta/countriesapp/model/Country.java
@@ -0,0 +1,40 @@
+package io.github.ardhiesta.countriesapp.model;
+
+/**
+ * Created by linuxluv on 09/11/17.
+ */
+
+public class Country {
+    // nama negara
+    String name;
+
+    // ibukota negara
+    String capital;
+
+    // alamat file gambar bendera
+    String flag;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+}
